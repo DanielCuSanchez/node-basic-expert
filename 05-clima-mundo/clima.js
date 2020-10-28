c6a477a6d3e1544480640076a734813e00014dc7
DanieÃl Cu <danielcu@alternet.com.mx>
2020-10-28 07:51:53 -0600

    API

--- 05-clima-mundo/clima.js
+++ 05-clima-mundo/clima.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 
+//API https://openweathermap.org/current
+
 const API_KEY = '74835d738b5362c91b3b5ab9a489aecb'
 const getClima = async (lat, lon)=>{
    const resp =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
